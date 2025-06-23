91671592ac39b1f396ffc47c2ab39015b803da50
binga288 <binga288@gmail.com>
2025-06-23 15:17:38 +0800

    更新 requirements.txt 以添加 boost，並在 setup.sh 中安裝 dlib 及調整目錄結構以便於克隆 nvdiffrast。

--- setup.sh
+++ setup.sh
@@ -2,9 +2,11 @@
 # install correct version of torch and torchvision according to your cuda version
 pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu117    
 pip install -r requirements.txt
+conda install -y -c conda-forge dlib=19.24.2
 pip install -e git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers
 pip install -e git+https://github.com/openai/CLIP.git@main#egg=clip
 pip install -e .
-cd eval_tool/Deep3DFaceRecon_pytorch_edit/nvdiffrast/
+cd eval_tool/Deep3DFaceRecon_pytorch_edit
 git clone https://github.com/NVlabs/nvdiffrast
+cd ./nvdiffrast
 pip install -e .
